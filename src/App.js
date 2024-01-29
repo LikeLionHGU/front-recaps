cd2462c814d9c3a5973c2a7b4e5eee6e1be0ffd0
yeji <yezi0512@handong.ac.kr>
2024-01-29 22:15:01 +0900

    first setting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Header from "./component/Header";
 import Write from "./component/Write";
+import List from "./component/List";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    // È­¸é ÀüÈ¯ÀÌ ÀÌ·ç¾îÁöµµ·Ï react-router-domÀ» »ç¿ë
+    // È­ï¿½ï¿½ ï¿½ï¿½È¯ï¿½ï¿½ ï¿½Ì·ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ react-router-domï¿½ï¿½ ï¿½ï¿½ï¿½
 
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header />}></Route>
         <Route path="/write" element={<Write />}></Route>
+        <Route path="list" element={<List />}></Route>
       </Routes>
     </BrowserRouter>
 
