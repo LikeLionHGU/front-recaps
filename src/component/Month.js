480ad8cfa8439cbf41c3e6bc4da427cf43f8d515
dkrehd0519 <dkrehd0519@naver.com>
2024-01-30 21:10:07 +0900

    feat: list page 연결

--- src/component/Month.js
+++ src/component/Month.js
@@ -19,14 +19,24 @@ export default function Month() {
       setMonth(1);
     }
   };
+  let num = month - 1;
+  if ((num = 1)) {
+    num = 12;
+  }
+  let num1 = month + 1;
+  if ((num = 12)) {
+    num = 1;
+  }
 
   return (
     <div className="month">
-      <Link to={"/list/" + month}>
+      <Link to={"/list/" + num}>
         <button onClick={decreaseMonth}>prev</button>
       </Link>
       <h1>{month} 월</h1>
-      <button onClick={increaseMonth}>next</button>
+      <Link to={"/list/" + num1}>
+        <button onClick={increaseMonth}>next</button>
+      </Link>
     </div>
   );
 }
